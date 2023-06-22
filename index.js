a6d354b4ecf1d037de891a209d41848b878639ec
Victor-Lis <victorlisbronzo1@gmail.com>
2023-06-22 20:49:33 -0300

    v1

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-const express = require("express")
-const server = express()
+const express = require("express");
+const server = express();
 const cors = require('cors');
 
-server.use(cors())
-// Query params = ?nome=NodeJS
-// Route Params = /linguagens/2
-// Request Body = { nome: "NodeJS", tipo: "Backend"}
+server.use(cors());
 
-const cursos = ["Node JS", "JavaScript", "React JS", "React Native JS", "Next JS", "Vue JS"]
+const cursos = ["Node JS", "JavaScript", "React JS", "React Native JS", "Next JS", "Vue JS"];
 
 server.get("/linguagens/:index", (req, res) => {
+  const { index } = req.params;
+  return res.json(cursos[index]);
+});
 
-    const { index } = req.params;
+const PORT = process.env.PORT || 3000;
 
-    return res.json(cursos[index])
-
-})
-
-server.listen(3000)
+server.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
